50eda524060cdd159eaca1fd43a8d5e35606a6a4
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-02-22 21:09:37 +0300

    init

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styleString from './string.module.css';
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <form className={styleString.form}>
+        <div className={styleString.wrapper}>
+          <Input 
+            placeholder={'Введите текст'}
+            type={'text'}
+            isLimitText={true}
+            maxLength={11}
+          />
+          <Button
+            text={'Развернуть'}
+          />
+        </div>
+        
+      </form>
     </SolutionLayout>
   );
 };
